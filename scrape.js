49e5cb8810c167e1346848931c7e81cd07a56edb
Jim Becker <beckerj03@gmail.com>
2023-06-12 12:57:36 -0400

    scrap and upload to s3

---
+++ scrape.js
@@ -0,0 +1,57 @@
+const axios = require('axios');
+const cheerio = require('cheerio');
+const { S3Client, PutObjectCommand } = require("@aws-sdk/client-s3");
+
+const url = 'https://minecraft-archive.fandom.com/wiki/Blocks/Gallery';
+
+async function main() {
+  const locations = await retrieveBlockLocations();
+  for (let location of locations) {
+    await uploadFileToS3(location.url, location.name)
+  }
+} 
+
+async function retrieveBlockLocations() {
+  const response = await axios.get(url)
+  const html = response.data;
+  const $ = cheerio.load(html);
+
+  const galleryItems = $('.wikia-gallery-item');
+
+  const urls = [];
+
+  galleryItems.each((index, element) => {
+    const image = $(element).find('.thumbimage');
+    const url = image.attr('data-src');
+    const name =image.attr('alt');
+    urls.push({name, url});
+  });
+  return urls;
+}
+
+async function uploadFileToS3(url, name) {
+  try {
+    const response = await axios.get(url, { responseType: "arraybuffer" });
+    const fileData = response.data;
+
+    const uploadParams = {
+      Bucket: 'rifik-backend-staging-cdn',
+      Key: `minecraft/images/${name}.png`,
+      Body: fileData,
+    };
+
+    const command = new PutObjectCommand(uploadParams);
+    
+    const s3Client = new S3Client({
+        region: 'us-east-1'
+    });
+    const result = await s3Client.send(command);
+
+    console.log("File uploaded successfully!", result);
+  } catch (error) {
+    console.error("Error uploading file:", error);
+  }
+}
+
+
+  main();
